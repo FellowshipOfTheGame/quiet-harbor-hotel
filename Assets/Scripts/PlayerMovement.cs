059354661a4e4c3e5c4012c2bb73d4fb05066e54
GRReiaya <113137419+GRReiaya@users.noreply.github.com>
2022-11-19 12:46:58 -0300

    First commit, .gitignore and started project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float moveSpeedMultiplier;
+    public float groundDrag;
+
+    public Transform orientation;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatIsGround;
+    bool grounded;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 movementDirection;
+    Rigidbody rb;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        //Ground Check
+        grounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, whatIsGround);
+
+        MyInput();
+        SpeedControl();
+
+        //Drag
+        if (grounded)
+            rb.drag = groundDrag;
+        else
+            rb.drag = 0;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void MovePlayer()
+    {
+        movementDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(movementDirection.normalized * moveSpeed * moveSpeedMultiplier, ForceMode.Force);
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatvel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatvel.magnitude > moveSpeed)
+        {
+            Vector3 limitedvel = flatvel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedvel.x, limitedvel.y, limitedvel.z);
+        }
+    }
+}
