64a575c9b4e40991a7344906c5b9abbbb5831d92
GRReiaya <113137419+GRReiaya@users.noreply.github.com>
2022-11-29 19:34:19 -0300

    UI and code improvements

---
+++ Assets/Scripts/MonsterMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class MonsterMovement : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform player;
+
+    public float killDistance = 10f;
+
+    void Update()
+    {
+        agent.SetDestination(player.position);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Cursor.lockState = CursorLockMode.Confined;
+            Cursor.visible = true;
+            SceneManager.LoadScene("GameOverScreen");
+            SceneManager.UnloadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
